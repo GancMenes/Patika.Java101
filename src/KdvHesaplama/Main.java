5ec6a724c9328c5c92545f9d90ccf53ac46fca40
GancMenes <ganc_menesyan@hotmail.com>
2022-03-21 10:15:03 +0300

    KDV Hesaplama

---
+++ src/KdvHesaplama/Main.java
@@ -0,0 +1,19 @@
+package KdvHesaplama;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the price");
+        double price = scanner.nextInt();
+
+        double kdv = price < 1000 ? price*0.18 : price*0.08;
+
+
+        System.out.println("KDV'siz tutar " + price);
+        System.out.println("KDV'li tutar = "+price+kdv);
+        System.out.println("KDV tutarı = " + kdv);
+    }
+}
---
+++ src/patika/dev/ortalamaHesaplama/Main.java
@@ -0,0 +1,33 @@
+package patika.dev.ortalamaHesaplama;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzık;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your math grade?");
+        mat = scanner.nextInt();
+
+        System.out.println("What is your fizik grade?");
+        fizik = scanner.nextInt();
+
+        System.out.println("What is your kimya grade?");
+        kimya = scanner.nextInt();
+
+        System.out.println("What is your turkce grade?");
+        turkce = scanner.nextInt();
+
+        System.out.println("What is your tarih grade?");
+        tarih = scanner.nextInt();
+
+        System.out.println("What is your muzık grade?");
+        muzık = scanner.nextInt();
+
+        int ortalama = (mat+fizik+kimya+turkce+tarih+muzık)/6;
+
+        String sonuc = ortalama < 60 ? "Kaldı" : "Gecti";
+    }
+}
