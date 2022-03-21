9beef124f14896affc680cc7633128938f098b3d
Ganc Menesyan <ganc_menesyan@hotmail.com>
2022-03-21 22:17:06 +0300

    Message

---
+++ src/SayilariSiralama/Main.java
@@ -0,0 +1,44 @@
+package SayilariSiralama;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x ,y ,z ;
+        int enBuyuk;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter x");
+        x  = scanner.nextInt();
+        System.out.println("Enter y");
+        y = scanner.nextInt();
+        System.out.println("Enter z");
+        z = scanner.nextInt();
+
+        enBuyuk = z;
+
+        if (x > y && x > z) {
+            if (y > z) {
+                System.out.println("x > y > z");
+            } else {
+                System.out.println("x > z > y");
+            }
+
+        }else if( y > x && y > z){
+                if (x > z){
+                    System.out.println("y > x > z");
+                }else {
+                    System.out.println("y > z > x");
+                }
+            }
+        else {
+            if (x < y) {
+                System.out.println("z > y > x");
+                }
+            else {
+                System.out.println("z > x > y");
+            }
+        }
+    }
+}
+
