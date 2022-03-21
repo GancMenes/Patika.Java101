a4cacb57f54eb9dd9abadc51dfc3fc7b159f534d
Ganc Menesyan <ganc_menesyan@hotmail.com>
2022-03-21 18:01:21 +0300

    Message

--- src/KosulluIfadeler/kjg/KullaniciGirisi/Main.java
+++ src/KosulluIfadeler/kjg/KullaniciGirisi/Main.java
@@ -14,30 +14,35 @@ public class Main {
         System.out.println("Şifreniz");
         password = scanner.nextLine();
 
-        if (userName.equals("patika") && password.equals("12345")){
+        if (userName.equals("patika") && password.equals("12345")) {
             System.out.println("Giriş yaptınız!");
-        }
-        else if(!userName.equals("patika") && password.equals("12345")){
+        } else if (!userName.equals("patika") && password.equals("12345")) {
             System.out.println("Kullanıcı adınızı yanlıs gırdınız");
-        }
-
-        else if (userName.equals("patika") && !password.equals("12345")) {
+        } else if (userName.equals("patika") && !password.equals("12345")) {
             System.out.println("Şifreniz yanlış!");
-            System.out.println("Şifrenizi sıfırlamak için 'Evet' e basın");
+            System.out.println("Şifrenizi sıfırlamak için 'Evet' e basın, tekrar denemek icin 'Hayır' a basın");
             String answer = scanner.nextLine();
-            if (answer.equals("Evet") && !answer.equals(null)){
-                System.out.println("Yeni şifrenizi giriniz");
-                int password2 = Integer.parseInt(scanner.nextLine());
 
-                if (password.equals(password2) ){
-                    System.out.println("Sifreniz eski şifreyle aynı olamaz");
-                }else {
-                    System.out.println("Yeni şifrenizi başarıyla girdiniz");
-                }
+
+            switch (answer) {
+
+                case "Evet" :
+                    System.out.println("Yeni şifreyi giriniz");
+
+                    int password2 = scanner.nextInt();
+
+                    if (password.equals(password2)) {
+                        System.out.println("Sifreniz eski şifreyle aynı olamaz");
+                    } else {
+                        System.out.println("Yeni şifrenizi başarıyla girdiniz");
+                    }
+
+                    break;
+
+                case "Hayır" :
+                    System.out.println("Bilgilerinizi kontrol ediniz");
+                    break;
             }
-        }
-        else {
-            System.out.println("Bilgilerinizi kontrol ediniz");
         }
     }
 }
