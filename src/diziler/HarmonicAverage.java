a39d2f3385ce1d5e5a9835ae75b9023bd9b27376
Ganc Menesyan <ganc_menesyan@hotmail.com>
2022-05-03 12:10:56 +0300

    Merge remote-tracking branch 'origin/Java101' into Java101

--- src/diziler/HarmonicAverage.java
+++ src/diziler/HarmonicAverage.java
@@ -1,6 +1,6 @@
 package diziler;
 
-public class harmonicAverage {
+public class HarmonicAverage {
     public static void main(String[] args) {
 
         int[] numbers  = {10, 20, 30, 40, 50, 60, 70};
